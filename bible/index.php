4ee1a22bb77d6596d3e7da42e6694d9918287d44
Wasd The Debugger ( NIKAS ) <93481198+wasdthedebugger@users.noreply.github.com>
2022-01-25 17:00:03 +0545

    Delete index.php

--- bible/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="refresh" content="0; url='poenhub.com'" />
-      <meta name="description" content="Entire Bible summary in one website. Spreading Zesus's Love <3">
-  </head>
-  <body>
-  </body>
-</html>
